12801d79d4cb0cceeddac819b5feded3de909f9c
BernardoSerraS <bserrinha@gmail.com>
2025-11-10 15:13:34 -0300

    CRUD de Categorias e Listagem de Clientes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import MainLayout from '../../src/Layout/MainLayout';
-import Home from '../../src/pages/Home/Index';
-import PedidoPage from '../../src/pages/Pedido/Index';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "../../src/Layout/MainLayout";
+import Home from "../../src/pages/Home/Index";
+import PedidoPage from "../../src/pages/Pedido/Index";
+import Categorias from "../pages/Categorias/Index";
+import Clientes from "../pages/Clientes/Index";
 
 const AppRoutes = () => {
   return (
@@ -10,6 +12,8 @@ const AppRoutes = () => {
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/pedido" element={<PedidoPage />} />
+        <Route path="/categorias" element={<Categorias />} />
+        <Route path="/clientes" element={<Clientes />} />
       </Route>
     </Routes>
   );
